43a565e65dbf6e347eafb038468c5e3c8ea51069
Moshe Zadka <zadka.moshe@gmail.com>
2020-08-01 17:08:47 -0700

    +stuff

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,35 @@
+#![feature(proc_macro_hygiene, decl_macro)]
+
+
+use rocket::http::RawStr;
+use serde::Serialize;
+use rocket_contrib::json::Json;
+
+
+#[macro_use] extern crate rocket;
+
+#[get("/")]
+fn index() -> &'static str {
+    "Hello, world!"
+}
+
+#[derive(Serialize)]
+struct Person {
+	name: String
+}
+
+impl Person {
+	fn new(name: &str) -> Person {
+		Person { name: name.into() }
+	}
+}
+
+#[get("/hello/<name>")]
+fn hello(name: &RawStr) -> Json<Person> {
+	let person = Person::new(name.as_str());
+	Json(person)
+}
+
 fn main() {
-    println!("Hello, world!");
+    rocket::ignite().mount("/", routes![index, hello]).launch();
 }
